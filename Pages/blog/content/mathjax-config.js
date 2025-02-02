d9282265d24a68e8a07da06b553d40ab91b655eb
David Michael Harper <66442656+DavidMichaelH@users.noreply.github.com>
2025-02-02 11:56:18 -0800

    MomementBound

---
+++ Pages/blog/content/mathjax-config.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      tags: 'all' // Automatically number all displayed equations
+    },
+    svg: {
+      fontCache: 'global'
+    }
+  };
+  
+
+
+  <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" async></script>  
+
+  
